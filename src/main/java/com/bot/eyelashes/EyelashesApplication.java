6e72cfff556f0c642aa28897cfa83707dff4bc56
Artem Bordiuzha <halp3ars@gmail.com>
2022-05-30 15:57:01 +0300

    init

---
+++ src/main/java/com/bot/eyelashes/EyelashesApplication.java
@@ -0,0 +1,13 @@
+package com.bot.eyelashes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyelashesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EyelashesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bot/eyelashes/EyelashesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.eyelashes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyelashesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
